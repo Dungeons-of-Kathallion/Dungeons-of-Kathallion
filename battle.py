cf37fc13e65f129baccb6098871c9462d36f5448
Nat Tabris <lumbar527@gmail.com>
2023-08-07 07:50:18 -0600

    Progress - call battle from main

--- battle.py
+++ battle.py
@@ -9,13 +9,13 @@ defend = 0
 turn = True
 
 #opens "save.yaml"
-with open("save.yaml") as f:
-    player = yaml.safe_load(f)
+# with open("save.yaml") as f:
+    # player = yaml.safe_load(f)
 
-if player["xp"] < 0:
-    player["xp"] = 0
+# if player["xp"] < 0:
+#     player["xp"] = 0
 
-def fight():
+def fight(player):
     global turn, enemy_health, defend, enemy_max, enemy_max_damage
     while player["health"] > 0:
         while turn:
@@ -24,6 +24,10 @@ def fight():
             if action.lower().startswith('a'):
                 if player["held item"] == "Sword":
                     enemy_health -= random.randint(0, 5)
+                    print("Enemy: ", enemy_health, "/", enemy_max)
+                    turn = False
+                if player["held item"] == "Axe":
+                    enemy_health -= random.randint(2, 7)
                     print("Enemy: ", enemy_health, "/", enemy_max)
                     turn = False
             elif action.lower().startswith('d'):
@@ -38,6 +42,9 @@ def fight():
                         player["max health"] += 5
                         player["health"] = player["max health"]
                         turn = False
+                    if item == "Axe":
+                        player["held item"] = "Axe"
+                        print("You are now holding an Axe")
         while not turn:
             if enemy_health > 0:
                 damage = random.randint(0, enemy_max_damage) - defend
@@ -49,54 +56,57 @@ def fight():
                 turn = True
             else:
                 player["xp"] += enemy_max * enemy_max_damage / 3
-                player["health"] += enemy_max_damage
-                enemy_max = random.randint(5, 21)
-                enemy_health = enemy_max
-                enemy_max_damage = random.randint(4, 8)
-                defend = 0
-                turn = True
+                player["health"] += random.randint(0, 3)
+                # enemy_max = random.randint(5, 21)
+                # enemy_health = enemy_max
+                # enemy_max_damage = random.randint(4, 8)
+                # defend = 0
+                # turn = True
+                still_playing = False
+                return
+    return
 
 
 
 still_playing = True
 
-while still_playing:
+# while still_playing:
 
-    fight()
+#     fight()
 
-    still_playing = False
+#     still_playing = False
 
-    if player["health"] <= 0:
-        if player["cheat"] < 3:
-            cheatcode = input("What is the not-die code? ")
-            if cheatcode == "4":
-                # cheat code was correct so keep playing
-                player["cheat"] += 1
-                player["health"] = 10
-                still_playing = True
-            else:
-                player["health"] = 10
-                player["max health"] = 10
-                player["x"] = 0
-                player["y"] = 0
-                player["inventory"] = ["Sword", "Healing Potion"]
-                player["held item"] = "Sword"
-                player["cheat"] = 0
-                player["xp"] -= 100
+#     if player["health"] <= 0:
+#         if player["cheat"] < 3:
+#             cheatcode = input("What is the not-die code? ")
+#             if cheatcode == "4":
+#                 # cheat code was correct so keep playing
+#                 player["cheat"] += 1
+#                 player["health"] = 10
+#                 still_playing = True
+#             else:
+#                 player["health"] = 10
+#                 player["max health"] = 10
+#                 player["x"] = 0
+#                 player["y"] = 0
+#                 player["inventory"] = ["Sword", "Healing Potion"]
+#                 player["held item"] = "Sword"
+#                 player["cheat"] = 0
+#                 player["xp"] -= 100
                 
-        else:
-            print("YOU DIED")
-            player["health"] = 10
-            player["max health"] = 10
-            player["x"] = 0
-            player["y"] = 0
-            player["inventory"] = ["Sword", "Healing Potion"]
-            player["held item"] = "Sword"
-            player["cheat"] = 0
-            player["xp"] -= 100
+#         else:
+#             print("YOU DIED")
+#             player["health"] = 10
+#             player["max health"] = 10
+#             player["x"] = 0
+#             player["y"] = 0
+#             player["inventory"] = ["Sword", "Healing Potion"]
+#             player["held item"] = "Sword"
+#             player["cheat"] = 0
+#             player["xp"] -= 100
 
 # put all the new data in the file
-dumped = yaml.dump(player)
+# dumped = yaml.dump(player)
 
-with open("save.yaml", "w") as f:
-    f.write(dumped)
+# with open("save.yaml", "w") as f:
+#     f.write(dumped)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import random
 import yaml
 import pickle
+import battle
 
 # varibles
 # room1x = random.randint
@@ -8,6 +9,7 @@ import pickle
 # says you are not playing.
 play = 0
 
+fought_enemy = False
 # opens "save.yaml"
 # with open("save.yaml") as f:
 #     player = yaml.safe_load(f)
@@ -19,6 +21,7 @@ if create_save.lower().startswith('n'):
     player = {
         "health":10,
         "max health":10,
+        "xp":0,
         "x":0,
         "y":0,
         "inventory":["Sword", "Healing Potion"],
@@ -46,6 +49,7 @@ if player["health"] <= 0 and play == 1:
         if not cheatcode == 43590:
             player["health"] = 10
             player["max health"] = 10
+            player["xp"] = 0
             player["x"] = 0
             player["y"] = 0
             player["inventory"] = ["Sword", "Healing Potion"]
@@ -59,6 +63,7 @@ if player["health"] <= 0 and play == 1:
         print("YOU DIED")
         player["health"] = 10
         player["max health"] = 10
+        player["xp"] = 0
         player["x"] = 0
         player["y"] = 0
         player["inventory"] = ["Sword", "Healing Potion"]
@@ -162,6 +167,83 @@ while play == 1:
         else:
             print("ERROR: That option is not allowed.")
 
+    elif player["x"] == 0 and player["y"] == -1:
+        which_direction = input(
+            "You can go north, east, and west.")
+        if which_direction.lower().startswith('n'):
+            player["y"] += 1
+        # elif which_direction.lower().startswith('s'):
+        #     player["y"] -= 1
+        if which_direction.lower().startswith('e'):
+            player["x"] += 1
+        if which_direction.lower().startswith('w'):
+            player["x"] -= 1
+        # elif which_direction.lower().startswith('t'):
+        #     if 'Axe' not in player["inventory"]:
+        #         player["inventory"].append('Axe')
+        elif which_direction.lower().startswith('m'):
+            if 'Map' in player["inventory"]:
+                print("**|**")
+                print("*[+]*")
+                print("**⊥**")
+        elif which_direction.lower().startswith('q'):
+            play = 0
+        else:
+            print("ERROR: That option is not allowed.")
+
+    elif player["x"] == -1 and player["y"] == -1:
+        which_direction = input(
+            "You can go east.")
+        # if which_direction.lower().startswith('n'):
+        #     player["y"] += 1
+        # elif which_direction.lower().startswith('s'):
+        #     player["y"] -= 1
+        if which_direction.lower().startswith('e'):
+            player["x"] += 1
+        # if which_direction.lower().startswith('w'):
+        #     player["x"] -= 1
+        # elif which_direction.lower().startswith('t'):
+        #     if 'Axe' not in player["inventory"]:
+        #         player["inventory"].append('Axe')
+        elif which_direction.lower().startswith('m'):
+            if 'Map' in player["inventory"]:
+                print("**|**")
+                print("*[+]*")
+                print("**⊥**")
+        elif which_direction.lower().startswith('q'):
+            play = 0
+        else:
+            print("ERROR: That option is not allowed.")
+
+    elif player["x"] == 1 and player["y"] == -1:
+        if fought_enemy == False:
+            which_direction = input(
+                "You can go west. There is an enemy here...preparing to fight!")
+            battle.fight(player)
+            fought_enemy = True
+        else:
+            which_direction = input(
+                "You can go west.")
+            # if which_direction.lower().startswith('n'):
+            #     player["y"] += 1
+            # elif which_direction.lower().startswith('s'):
+            #     player["y"] -= 1
+            # if which_direction.lower().startswith('e'):
+            #     player["x"] += 1
+            if which_direction.lower().startswith('w'):
+                player["x"] -= 1
+            # elif which_direction.lower().startswith('t'):
+            #     if 'Axe' not in player["inventory"]:
+            #         player["inventory"].append('Axe')
+            elif which_direction.lower().startswith('m'):
+                if 'Map' in player["inventory"]:
+                    print("**|**")
+                    print("*[+]*")
+                    print("**⊥**")
+            elif which_direction.lower().startswith('q'):
+                play = 0
+            else:
+                print("ERROR: That option is not allowed.")
     else:
         print("ERROR: You have traveled out of bounds.")
         print("Teleporting to start...")

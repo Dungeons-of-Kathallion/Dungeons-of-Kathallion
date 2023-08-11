e13ac2561a8a10b70063fef6effa3cded6b303e8
Cromha <87318892+OcelotWalrus@users.noreply.github.com>
2023-08-11 16:18:53 +0200

    progress - update UI dialog (#6)

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ with open("items.yaml") as f:
 with open("start.yaml") as f:
     start_player = yaml.safe_load(f)
 
-create_save = input("Do you want to [o]pen saved game or create [n]ew game? ")
+save_selection = input("Do you want to [o]pen saved game, create [n]ew game or [d]elete an existing save? ")
 
-if create_save.lower().startswith('n'):
+if save_selection.lower().startswith('n'):
     enter_save_name = input("Please name your save: ")
     player = start_player
     dumped = yaml.dump(player)
@@ -30,16 +30,34 @@ if create_save.lower().startswith('n'):
         f.write(dumped)
     save_file = save_name
     play = 1
-elif create_save.lower().startswith('o'):
+elif save_selection.lower().startswith('o'):
     open_save = input("Please choose a save to open: ")
     save_file = "save_" + open_save + ".yaml"
     check_file = os.path.isfile(save_file)
     if check_file == False:
         print("ERROR: Couldn't find save file '" + save_file + "'")
-        exit(1)
+        play = 0
     with open(save_file) as f:    
         player = yaml.safe_load(f)
     play = 1
+elif save_selection.lower().startswith('d'):
+    delete_save = input("Please choose a save to delete: ")
+    save_file = "save_" + delete_save + ".yaml"
+    check_file = os.path.isfile(save_file)
+    if check_file == False:
+        print("ERROR: Couldn't find save file '" + save_file + "'")
+        play = 0
+        exit(1)
+    with open(save_file) as f:
+        verify = input("Are you sure you want to delete the following save (y/n)? ")
+        if verify == "y":
+            player = "placeholder, do not delete"
+            os.remove(save_file)
+            play = 0
+        if verify == "n":
+            player = "placeholder, do not delete"
+            print("Aborting current process...")
+            play = 0
 else:
     print("ERROR: That option is not allowed.")
 
@@ -51,25 +69,54 @@ def search(x, y):
         point_x, point_y = point_i["x"], point_i["y"]
         # print(i, point_x, point_y, player)
         if point_x == player["x"] and point_y == player["y"]:
-            # print(i)
             map_location = i
             return map_location
 
+def search_specific_x():
+    global map_location_x
+    for i in range(0, map["coordinate count"]):
+        point_i = map["point" + str(i)]
+        point_x = point_i["x"]
+        if point_x == player["x"]:
+            map_location_x = i
+            return map_location_x
+
+def search_specific_y():
+    global map_location_y
+    for i in range(0, map["coordinate count"]):
+        point_i = map["point" + str(i)]
+        point_y = point_i["x"]
+        if point_y == player["y"]:
+            map_location_y = i
+            return map_location_y
+
 # gameplay here:
 def run(play):
+    separator = "###############################"
+    print(separator)
     print("Reserved keys:")
     print("N: Go north")
     print("S: Go south")
     print("E: Go east")
     print("W: Go west")
+    print("I: View items. When in this view, type the name of an item to examine it.")
+    print("Q: Quit game")
+    print(" ")
+    print("Hints:")
     print("If you find an item on the ground, type the name of the item to take it.")
-    print("I: view items. When in this view, type the name of an item to examine it.")
     print("Some items have special triggers, wich will often be stated in the description. Others can only be activated in certain situations, like in combat.")
+    print(" ")
     # Mapping stuff
 
     while play == 1:
         global player
         map_location = search(player["x"], player["y"])
+        map_location_x = search_specific_x()
+        map_location_y = search_specific_y()
+        print("Coordinates:")
+        print("X: " + str(map_location_x))
+        print("Y: " + str(map_location_y))
+        print(" ")
         if "North" not in map["point" + str(map_location)]["blocked"]:
             print("You can go North")
         if "South" not in map["point" + str(map_location)]["blocked"]:
@@ -117,7 +164,9 @@ def run(play):
                     player = start_player
                     play = 0
                     return play
-        command = input("What will you do?")
+        print(" ")
+        command = input("What will you do?\n")
+        print(" ")
         if command.lower().startswith('go'):
             print("Rather than saying Go <direction>, simply say <direction>.")
         elif command.lower().startswith('n'):
@@ -143,7 +192,17 @@ def run(play):
         elif command.lower().startswith('i'):
             which_item = input("You have these items in your inventory: " + str(player["inventory"]) + " ")
             if which_item in player["inventory"]:
-                print(item[which_item]["description"])
+                print(" ")
+                print("Name: " + which_item)
+                print("Type: " + item[which_item]["type"])
+                print("Description: " + item[which_item]["description"])
+                if item[which_item]["type"] == "Weapon":
+                    print("Damage: " + str(item[which_item]["damage"]))
+                    print("Defense: " + str(item[which_item]["defend"]))
+                if item[which_item]["type"] == "Consumable":
+                    print("Max Bonus: " + str(item[which_item]["max bonus"]))
+                    print("Healing Level: " + str(item[which_item]["healing level"]))
+                print(" ")
             else:
                 print("You do not have that item.")
         elif command.lower().startswith('m'):
@@ -159,6 +218,7 @@ def run(play):
             else:
                 print("You already have that item.")
         elif command.lower().startswith('q'):
+            print(separator)
             play = 0
             return play
 
@@ -171,3 +231,4 @@ dumped = yaml.dump(player)
 save_file_quit = save_file
 with open(save_file_quit, "w") as f:
     f.write(dumped)
+

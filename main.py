a6d49d48016756419817b6683bddfdafa889b5a9
Cromha <enderman.quickly@gmail.com>
2023-08-18 19:50:00 +0200

    Fixes

--- main.py
+++ main.py
@@ -301,6 +301,7 @@ def run(play):
                 if map["point" + str(map_location)]["enemy_type"] == "black":
                     list_enemies = ['Black Orc', 'Dark Marksman', 'Doomed Horror']
 
+            if player["health"] > 0:
                 choose_rand_enemy = random.randint(0, len(list_enemies) - 1)
                 choose_rand_enemy = list_enemies[choose_rand_enemy]
                 choosen_enemy = enemy[choose_rand_enemy]
@@ -319,7 +320,7 @@ def run(play):
                     else:
                         player["inventory"].append(choosen_item)
                         player["taken items"].append(map_location)
-            if player["health"] > 0:
+                print(" ")
                 player["defeated enemies"].append(map_location)
                 if "North" not in map["point" + str(map_location)]["blocked"]:
                     print("You can go North")
